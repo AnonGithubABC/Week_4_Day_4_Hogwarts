ed751a2cd08312d1ccfc8b73d33827e79784fe24
Robert Henderson <robert@nterpryz.com>
2017-12-14 11:09:51 +0000

    part 3 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
+require('pry-byebug')
 require_relative ('../models/student')
+
+Student.delete_all()
 
 student1 = Student.new({
   "first_name" => "Harry",
@@ -7,4 +10,41 @@ student1 = Student.new({
   "age" => 11
   })
 
+student2 = Student.new({
+  "first_name" => "Ron",
+  "last_name" => "Weasley",
+  "house" => "Gryfindor",
+  "age" => 11
+  })
+
+student3 = Student.new({
+  "first_name" => "Hermoine",
+  "last_name" => "Granger",
+  "house" => "Gryfindor",
+  "age" => 12
+  })
+
+student4 = Student.new({
+  "first_name" => "Draco",
+  "last_name" => "Malfoy",
+  "house" => "Slytherin",
+  "age" => 12
+  })
+
+student5 = Student.new({
+  "first_name" => "Luna",
+  "last_name" => "Lovegood",
+  "house" => "Hufflepuff",
+  "age" => 13
+  })
+
 student1.save
+student2.save
+student3.save
+student4.save
+student5.save
+
+# student5.delete
+
+binding.pry
+nil
--- models/student.rb
+++ models/student.rb
@@ -22,7 +22,32 @@ class Student
           values = [@first_name, @last_name, @house, @age]
           student_data = SqlRunner.run(sql, values)
           @id = student_data.first()['id'].to_i
+  end
 
+  def delete()
+    sql = "DELETE FROM students WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  #--------------- CLASS METHODS -----------------#
+
+  def self.delete_all()
+    sql = "DELETE FROM students;"
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM students"
+    students = SqlRunner.run(sql)
+    result = students.map{ |student| Student.new (student)}
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM students WHERE id = $1"
+    values = [id]
+    student = SqlRunner.run(sql, values)
+    result = Student.new(student[0])
   end
 
 
